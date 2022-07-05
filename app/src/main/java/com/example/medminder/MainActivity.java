3358b5969100f88a58e4adaf1c16b3ed48802cf3
heryanh <ryan_he@sfu.ca>
2022-07-05 13:50:16 -0700

    test

--- app/src/main/java/com/example/medminder/MainActivity.java
+++ app/src/main/java/com/example/medminder/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
+    //
 
 
 
