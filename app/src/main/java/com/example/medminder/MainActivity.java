92fe8bb0d90d0507e703ce072a178b9395b2092c
ziyingp <ziyingp@sfu.ca>
2022-07-06 23:40:25 -0700

    Medminder

--- app/src/main/java/com/example/medminder/MainActivity.java
+++ app/src/main/java/com/example/medminder/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.medminder;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.medminder.ui.home.HomeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -12,7 +19,8 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.medminder.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     private ActivityMainBinding binding;
 
@@ -32,6 +40,45 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        HomeFragment homeFragment=new HomeFragment();
+        FragmentManager manager=getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.home_layout,homeFragment).commit();
+
+        Button btn = (Button) findViewById(R.id.testbtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog();
+            }
+        });
     }
+
+    public void Dialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Are you sure? ");
+        builder.setTitle("Just Make Sure!!");
+        builder.setCancelable(false);
+
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast toast = Toast.makeText(getApplicationContext(), "you click yes", Toast.LENGTH_SHORT);
+            }
+        });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+
+
 
 }
--- app/src/main/java/com/example/medminder/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/medminder/ui/home/HomeFragment.java
@@ -2,36 +2,133 @@ package com.example.medminder.ui.home;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
+import androidx.fragment.app.FragmentActivity;
+import com.example.medminder.R;
 import com.example.medminder.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class HomeFragment extends Fragment implements AdapterView.OnItemClickListener
+{
+
+    ListView listViewData;
+    ArrayAdapter<String> adapter;
+
 
     private FragmentHomeBinding binding;
-
+@Nullable
+@Override
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
+                             ViewGroup container, Bundle savedInstanceState)
+    {
+       /* HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        *//*final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);*//*
+        listViewData= getView().findViewById(R.id.listView_data);
+        adapter=new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_multiple_choice,MedicationArray);
+        listViewData.setAdapter(adapter);
+        return root;*/
+
+        View view=inflater.inflate(R.layout.fragment_home,container,false);
+        return view;
     }
+   /* @Override
+    public void onViewCreated(@NonNull View view,@Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view,savedInstanceState);
+        String[] MedicationArray={"medication1","medication2","medication3","medication4","medication5","medication6"};
+        ListView listView=(ListView)view.findViewById(R.id.listView_data);
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_list_item_1,MedicationArray);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
+*/
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getActivity().getMenuInflater().inflate(R.menu.main_menu,menu);
+        //super.onCreateOptionsMenu(menu,inflater);
+        return true;
+
+
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        int id= item.getItemId();
+        if(id==R.id.item_done)
+        {
+            String itemSelected="Selected items: \n";
+            for(int i=0;i<listViewData.getCount();i++)
+            {
+                if(listViewData.isItemChecked(i))
+                {
+                    itemSelected += listViewData.getItemAtPosition(i) + "\n";
+                }
+            }
+            Toast.makeText(getActivity(),itemSelected,Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id)
+    {
+        if(position==0)
+            Toast.makeText(getActivity(),"medication1",Toast.LENGTH_SHORT).show();
+        if(position==1)
+            Toast.makeText(getActivity(),"medication2",Toast.LENGTH_SHORT).show();
+        if(position==2)
+            Toast.makeText(getActivity(),"medication3",Toast.LENGTH_SHORT).show();
+        if(position==3)
+            Toast.makeText(getActivity(),"medication4",Toast.LENGTH_SHORT).show();
+        if(position==4)
+            Toast.makeText(getActivity(),"medication5",Toast.LENGTH_SHORT).show();
+        if(position==5)
+            Toast.makeText(getActivity(),"medication6",Toast.LENGTH_SHORT).show();
+    }
+
+
 }
